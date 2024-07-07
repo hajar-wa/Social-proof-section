81e94ddb917b7ff2c5e1fe58dd929461a19fff8f
Hajar <hajarwahid@yahoo.com>
2024-07-07 20:41:26 +0100

    adding files

---
+++ data.js
@@ -0,0 +1,23 @@
+const info = [
+    {
+        img:  `<img src="./images/image-colton.jpg" alt="image-colton"  class = "avatar-img">`,
+        name: "Colton Smith ",
+        feedback:  `"We needed the same printed design as the one we had ordered a week prior. Not only did they find the original order, but we also received it in time.Excellent!"`
+    },
+    {
+        img:  `<img src="./images/image-irene.jpg" alt="image-irene" class = "avatar-img">`,
+        name: " Irene Roberts ",
+        feedback : `"Customer service is always excellent and very quick turn around. Completely delighted with the simplicity of the purchase and the speed of delivery."`
+    },
+    {
+        img:  `<img src="./images/image-anne.jpg" alt="image-anne" class = "avatar-img">`,
+        name: "Anne Wallace ",
+        feedback: `"Put an order with this company and can only praise them for the very highstandard. Will definitely use them again and recommend them to everyone!"`
+    }
+]
+
+
+const names = ["Reviews", "Report Guru", "BestTech"]
+ 
+
+export {info, names}
